9ce795de4cd221874299d81b0eca433d8c85664d
LukasWolff2002 <lwolff@miuandes.cl>
2025-06-11 15:23:05 -0400

    avance

--- CODIGO/eigenvalues.py
+++ CODIGO/eigenvalues.py
@@ -1,18 +1,17 @@
 import numpy as np
 
-
 #Defino los datos conocidos del sistema
-m1 = 0
-m2 = 0
-m3 = 0
+m1 = 100 
+m2 = 100
+m3 = 100
 
 c1 = 0
 c2 = 0
 c3 = 0
 
-k1 = 0
-k2 = 0
-k3 = 0
+k1 = 100 #Rigidez total por piso
+k2 = 200
+k3 = 100
 
 
 #Las matrices a ensamblar son:
@@ -28,6 +27,10 @@ K = np.array([[k1, -k1, 0],
              [-k1, k1+k2, -k2],
              [0, -k2, k2+k3]])
 
+r = np.array([[1],
+              [1],
+              [1]])
+
 #Defino las frecuencias fundamentales del sistema, ademas de los modos de vibracion
 #Los cuales son los valores propios de la matriz de masa por la matriz de rigidez
 
@@ -41,3 +44,35 @@ def eigenvalues(M, K):
     eigenvectors = eigenvectors[:, idx]
     
     return eigenvalues, eigenvectors
+
+def normalizar_vectores(phi):
+
+    A = phi.T@M@r
+    B = phi.T@M@phi
+
+    return phi * (A/B)
+
+def normalizar_matrices (phi, matriz):
+
+    return phi.T @ matriz @ phi
+
+valores, vectores = eigenvalues(M, K)
+
+wn = np.sqrt(valores)  # Frecuencias naturales
+
+#Ahora normalizo los vectores
+
+phi1, phi2, phi3 = vectores[:, 0], vectores[:, 1], vectores[:, 2]
+
+phi1_norm = normalizar_vectores(phi1)
+phi2_norm = normalizar_vectores(phi2)
+phi3_norm = normalizar_vectores(phi3)
+
+Phi_norm = np.array([phi1_norm, phi2_norm, phi3_norm])
+
+#Ahora normalizo las matrices
+M_norm = normalizar_matrices(Phi_norm, M)
+K_norm = normalizar_matrices(Phi_norm, K)
+
+#Bien, ahora 
+
